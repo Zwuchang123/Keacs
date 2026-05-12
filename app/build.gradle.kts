43050122a8c5084865068bdca0217f476631b91c
zwuchang <1414593178@qq.com>
2026-05-12 12:37:53 +0800

    chore: 准备发布 v1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.keacs.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.4.1"
+        versionCode = 16
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
